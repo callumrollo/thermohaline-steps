2f156c880bff1176c2f611d8ac6ab415338db756
Callum Rollo <c.rollo@outlook.com>
2021-03-17 11:16:31 +0000

    corrected ml classification and gl exclusion

--- detect_staircases.py
+++ detect_staircases.py
@@ -36,7 +36,7 @@ def add_ml_stats_row(stats_df, df):
     return stats_df
 
 
-def classify_staircase(p, ct, sa, ml_grad=0.0005, ml_density_difference=0.005, av_window=200, height_ratio=30):
+def classify_staircase(p, ct, sa, ml_grad=0.005, ml_density_difference=0.05, av_window=200, interface_max_height=30):
     """
     all data should be at 1 dbar resolution (for now)
     Notes:
@@ -86,8 +86,8 @@ def classify_staircase(p, ct, sa, ml_grad=0.0005, ml_density_difference=0.005, a
     df.loc[np.abs(df_smooth.beta * df_diff.sa * 1028) < ml_grad, 'mixed_layer_sal_mask'] = False
     df['mixed_layer_sigma_mask'] = True
     df.loc[np.abs(df_diff.sigma1) < ml_grad, 'mixed_layer_sigma_mask'] = False
-    # Combine logical masks for a mask that is False for mixed layer detected in temp, salt or density
-    df['mixed_layer_mask'] = df.mixed_layer_temp_mask & df.mixed_layer_sal_mask & df.mixed_layer_sigma_mask
+    # Combine logical masks for a mask. Mixed layers must satisfy criteria for temperature, salinity and density
+    df['mixed_layer_mask'] = df.mixed_layer_temp_mask | df.mixed_layer_sal_mask | df.mixed_layer_sigma_mask
 
     # Create dataframe of only points within mixed layers
     df_ml = df[~df.mixed_layer_mask]
@@ -125,26 +125,51 @@ def classify_staircase(p, ct, sa, ml_grad=0.0005, ml_density_difference=0.005, a
     # Drop mixed layers with density difference exceeding ml_density_difference TODO better value here
     df_ml_stats = df_ml_stats[df_ml_stats.sigma1_range < ml_density_difference]
     """
-    Step 2 Assess gradient layers  between mixed layers
+    Step 2  Assess gradient layers between mixed layers and calculate their properties
     """
+    # Create empty dataframe with same columns and df_ml_stats
     df_gl_stats = pd.DataFrame(columns=df_ml_stats.columns)
     prev_row = df_ml_stats.iloc[0]
+    # Loop through mixed layer stats. All layers between mixed layers initially classified as gradient layers
     for i, row in df_ml_stats.iloc[1:].iterrows():
         gl_rows = df[(df.p >= prev_row.p_end) & (df.p <= row.p_start)]
         df_gl_stats = add_ml_stats_row(df_gl_stats, gl_rows)
         prev_row = row
     print('done')
 
-    # Step 3 measure height of interface and their properties
+    # Exclude gradient layers that are thicker than the max height, or thicker than adjacent mixed layers
+    df_gl_stats['adj_ml_height'] = np.maximum.reduce([df_ml_stats.iloc[1:].pressure_extent.values, df_ml_stats.iloc[:-1].pressure_extent.values])
+    df_gl_stats['height_ratio'] = df_gl_stats.adj_ml_height / df_gl_stats.pressure_extent
+    df_gl_stats = df_gl_stats[df_gl_stats.pressure_extent < df_gl_stats.adj_ml_height]
+    df_gl_stats = df_gl_stats[df_gl_stats.pressure_extent < interface_max_height]
+
+
+    """
+    Step 3
+    """
+
     # Step 4 Classify layers in the double diffusive regime as salt finger (sf) or diffusive convection (dc)
     # Step 5 Identify sequences of steps
 
     # temporary tests of functionality
+
+    # Create masks of mixed layers and gradient layers
+    df['mixed_layer_final_mask'] = True
+    for i, row in df_ml_stats.iterrows():
+        df.loc[row.p_start:row.p_end, 'mixed_layer_final_mask'] = False
+
+    df['gradient_layer_final_mask'] = True
+    for i, row in df_gl_stats.iterrows():
+        df.loc[row.p_start:row.p_end, 'gradient_layer_final_mask'] = False
+
     import matplotlib.pyplot as plt
     fig, ax = plt.subplots()
     ax.plot(df.ct, df.p, color='gray', alpha=0.5)
-    for i, row in df_ml_stats.iterrows():
-        ax.plot([row.ct, row.ct], [row.p_start, row.p_end], color='C0')
+    #np.ma.array(ct[i, :] + 1.1 * align - 3, mask=ml_sf.mask[i, :])
+    ax.plot(np.ma.array(df.ct, mask=df['mixed_layer_final_mask']), np.ma.array(df.p, mask=df['mixed_layer_final_mask']), color='C0')
+    ax.plot(np.ma.array(df.ct, mask=df['gradient_layer_final_mask']), np.ma.array(df.p, mask=df['gradient_layer_final_mask']), color='C1')
+    #for i, row in df_ml_stats.iterrows():
+    #    ax.plot([row.ct, row.ct], [row.p_start, row.p_end], color='C0')
     #for i, row in df_gl_stats.iterrows():
     #    ax.plot([row.ct, row.ct], [row.p_start, row.p_end], color='C1')
 
