01be83588a9b030cc77f8df7f82c6bce11476c5e
Callum Rollo <c.rollo@outlook.com>
2025-07-18 15:43:16 +0200

    fix tests to work remotely and add conda env

--- test_staircase.py
+++ test_staircase.py
@@ -6,11 +6,13 @@ reject/fix non uniform data
 data with gaps
 """
 import sys
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-sys.path.append('/media/callum/storage/Documents/Eureka/processing/staircase_experiment/reimplement/')
-from detect_staircases import classify_staircase
+folder = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, folder)
+from detect_staircases import classify_staircase, identify_staircases_from_layers
 
 
 p = np.arange(1000)
@@ -23,6 +25,20 @@ for center in np.arange(0, 850, 100):
     ct[center - span:center + span] = np.mean(ct_orig[center - span:center + span])
     sa[center - span:center + span] = np.mean(sa_orig[center - span:center + span])
 
+def plotter(df, mixes, grads):
+    fig, ax = plt.subplots(1, 2, figsize=(18, 10), sharey='row')
+    ax[0].plot(df.ct, df.p, color='gray', alpha=0.2)
+    ax[0].plot(np.ma.array(df.ct, mask=df['mixed_layer_salt_finger_mask']),
+               np.ma.array(df.p, mask=df['mixed_layer_salt_finger_mask']), color='C0')
+    ax[0].plot(np.ma.array(df.ct, mask=df['gradient_layer_salt_finger_mask']),
+               np.ma.array(df.p, mask=df['gradient_layer_salt_finger_mask']), color='C1')
+
+    ax[1].plot(df.sa, df.p, color='gray', alpha=0.2)
+    ax[1].plot(np.ma.array(df.sa, mask=df['mixed_layer_salt_finger_mask']),
+               np.ma.array(df.p, mask=df['mixed_layer_salt_finger_mask']), color='C0')
+    ax[1].plot(np.ma.array(df.sa, mask=df['gradient_layer_salt_finger_mask']),
+               np.ma.array(df.p, mask=df['gradient_layer_salt_finger_mask']), color='C1')
+    plt.close('all')
 
 def test_ideal():
     # Ideal dataset should contain 8 mixed layers and 7 gradient layers
@@ -75,7 +91,7 @@ def test_spacing_close():
 
 def test_vanderboog_argo():
     # checking against the Argo data from vanderBoog paper
-    vdb = pd.read_csv('vanderboog_argo_demo_data.csv')
+    vdb = pd.read_csv(f'{folder}/data/vanderboog_argo_demo_data.csv')
     vdb = vdb.loc[:1000, :]
     df, mixes, grads = classify_staircase(vdb.pressure, vdb.conservative_temperature,
                                           vdb.absolute_salinity, layer_height_ratio=0.9, ml_density_difference=0.0048)
@@ -84,19 +100,7 @@ def test_vanderboog_argo():
     assert mixes.p_end.max() < 950
     assert len(mixes) == 9
     assert len(grads) == 8
+    staircases = identify_staircases_from_layers(df, mixes, grads)
+    assert len(staircases) == 2
 
 
-def plotter(df, mixes, grads):
-    fig, ax = plt.subplots(1, 2, figsize=(18, 10), sharey='row')
-    ax[0].plot(df.ct, df.p, color='gray', alpha=0.2)
-    ax[0].plot(np.ma.array(df.ct, mask=df['mixed_layer_salt_finger_mask']),
-               np.ma.array(df.p, mask=df['mixed_layer_salt_finger_mask']), color='C0')
-    ax[0].plot(np.ma.array(df.ct, mask=df['gradient_layer_salt_finger_mask']),
-               np.ma.array(df.p, mask=df['gradient_layer_salt_finger_mask']), color='C1')
-
-    ax[1].plot(df.sa, df.p, color='gray', alpha=0.2)
-    ax[1].plot(np.ma.array(df.sa, mask=df['mixed_layer_salt_finger_mask']),
-               np.ma.array(df.p, mask=df['mixed_layer_salt_finger_mask']), color='C0')
-    ax[1].plot(np.ma.array(df.sa, mask=df['gradient_layer_salt_finger_mask']),
-               np.ma.array(df.p, mask=df['gradient_layer_salt_finger_mask']), color='C1')
-    plt.show()
